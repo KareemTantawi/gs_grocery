44be9b852d36d53de9d41ecdb7a93a3412167a22
Kareem Tantawi <kareem01229526319@gmail.com>
2024-11-22 16:19:07 +0200

    make some changes in the application

--- lib/gc_grocery.dart
+++ lib/gc_grocery.dart
@@ -1,27 +1,13 @@
-// import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:gs_grocery/core/routes/app_route.dart';
-
-import 'core/routes/routes.dart';
 
 class GcGrocery extends StatelessWidget {
-  GcGrocery({super.key, required this.appRouter});
-  AppRouter appRouter;
+  const GcGrocery({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        // useInheritedMediaQuery: true,
-        // locale: DevicePreview.locale(context),
-        // builder: DevicePreview.appBuilder,
-        // home: Onboarding1(),
-        onGenerateRoute: appRouter.generateRoute,
-        initialRoute: Routes.onBoardingScreen,
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-// import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:gs_grocery/core/routes/app_route.dart';
 import 'package:gs_grocery/gc_grocery.dart';
 
 void main() {
   runApp(
-    GcGrocery(
-      appRouter: AppRouter(),
-    ),
+    const GcGrocery(),
   );
 }
